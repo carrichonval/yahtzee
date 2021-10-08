970184497b255f2a0a5ef0f7864d8b0a8a2aaaa0
Valentinc <valentin.carrichon@outlook.com>
2021-10-08 09:56:06 +0200

    Prod lancer de

--- lib/tests/yahtzee/yahtzee.ts
+++ lib/tests/yahtzee/yahtzee.ts
@@ -5,4 +5,14 @@ export class Yahtzee{
         return Math.floor(Math.random() * 6)+1;    
     }
 
+    static lancer() : Array<number>
+    {
+        let tab = []
+        for (let i = 1; i <=5 ; i++) {
+            tab.push(this.lanceUnDe())
+        }
+        console.log(tab)
+        return tab
+    }
+
 }
